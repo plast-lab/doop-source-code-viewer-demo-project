90528a7bbde24e123403575f4b696f7c73b6edb9
Anastasios Antoniadis <anantoni@di.uoa.gr>
2016-01-15 16:59:30 +0200

    Add inner classes to demo

--- src/main/java/extras/Test.java
+++ src/main/java/extras/Test.java
@@ -13,11 +13,13 @@ public class Test {
 
     public void speak() {
         System.out.println("Test: speak()");
+        print(true);
     }
 
     public void speak(Test2 testParam) {
         System.out.println("Test: speak(Test2)");
         Test2 testLocal = testParam;
+        print(false);
     }
 
     public void speak(Test2 test1, boolean flag, Test2[] test2) {
@@ -28,6 +30,30 @@ public class Test {
         }
         else {
             test2[5].speak();
+        }
+        print(true);
+    }
+
+    public void print(boolean flag) {
+        A a;
+        if (flag) {
+            a = new A();
+        }
+        else {
+            a = new B();
+        }
+        a.foo();
+    }
+
+    public class A {
+        public void foo() {
+            System.out.println("A::foo");
+        }
+    }
+
+    public class B extends A {
+        public void foo() {
+            System.out.println("B::foo");
         }
     }
 }
